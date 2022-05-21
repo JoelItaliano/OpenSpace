5203e0ad680fab1f3ad9bfa4649122dbee1fe124
JoelItaliano <92814212+JoelItaliano@users.noreply.github.com>
2022-05-21 17:38:00 -0300

    agregando los cambios finales para lograr pasar las pruebas

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let btnOk = document.getElementById("button__ok");
 let txtPass = document.getElementById("password");
 let imgCohete = document.getElementById('rocket')
 let activarFunClickCheckbox = false;
+let detenerIntervalo //esta variable es llamada es la funcion para detener el coehete
 
 
 
@@ -21,12 +22,41 @@ btnOk.addEventListener('click', function (){
         }
 })
 
+
 /*funcion que se activa al presionar el boton de lanzamiento*/
 btnLaunch.addEventListener("click", function (){
     alert('despegando')
-    setInterval(moverCohete,50,); //llama a la función para mover el cohete y le asigna el intervalo de tiempo que lo va a llamar
+    detenerIntervalo = setInterval(moverCohete,20,); //llama a la función para mover el cohete y le asigna el intervalo de tiempo que lo va a llamar
+
 })
 
+
+/*Funcion que toma cuando se hace click dentro de la caja de texto*/
+function clickCheckbox(){
+    if (activarFunClickCheckbox === true) { // si está activada la funcion activa las cajas y el ranGo
+        let checkOK = controlCheck(); //llama a la funcion para controlar que esten activadas las 6 casillas
+        let rangeOk = controlRango(); // llama a la fun para controlar que este en 100 todos los input de rango
+        if (checkOK ==='true' && rangeOk === 'true') { // si ambos da true entra
+            btnLaunch.style.transform = 'scale(1.10)'
+            btnLaunch.removeAttribute('disabled'); //activa el boton de lanzamiento
+            /*addDisabled(checkbox); //desactiva las caja de check y rango
+            addDisabled(range);*/
+          /*  activarFunClickCheckbox = false //vuelve a poner en falso la variable para ya no entre a la función cada vez que se da click*/
+        }
+    }
+}
+
+/*funcion para mover el cohete*/
+function moverCohete() {
+    let desplazar = obtenerPositionCohete() //llamna a la funcion y lo que retorna lo asigna a una variable
+    if (desplazar < 1000) { //si el cohete llega a los 100px llama a la función lo detiene
+        desplazar += 5; //cada vez que el intervalo de tiempo llama a la función le agrega un px
+        imgCohete.style.bottom = desplazar + 'px'; //agrega la nueva medida al estilo botton, esto hace que de efecto de movimiento cada vez que es llamada la funcion
+    }else {
+        clearInterval(detenerIntervalo)
+    }
+    console.log(desplazar)//es para ver cual es la nueva posicion de cohete
+}
 
 /*Funcion que devuelve la poosicion donde se encuentra el cohete*/
 function obtenerPositionCohete() {
@@ -39,30 +69,7 @@ function obtenerPositionCohete() {
     return desplazar
 }
 
-/*funcion para mover el cohete*/
-function moverCohete() {
-    let desplazar = obtenerPositionCohete() //llamna a la funcion y lo que retorna lo asigna a una variable
-    desplazar += 1; //cada vez que el intervalo de tiempo llama a la función le agrega un px
-    imgCohete.style.bottom = desplazar + 'px'; //agrega la nueva medida al estilo botton, esto hace que de efecto de movimiento cada vez que es llamada la funcion
-    console.log(desplazar)//es para ver cual es la nueva posicion de cohete
-}
-
-/*Funcion que toma cuando se hace click dentro de la caja de texto*/
-function clickCheckbox(){
-    if (activarFunClickCheckbox === true) { // si esta activada la funcion activa las cajas y el ranfo
-        let checkOK = controlCheck(); //llama a la funcion para controlar que esten activadas las 6 casillas
-        let rangeOk = controlRango(); // llama a la fun para controlar que este en 100 todos los input de rango
-        if ((checkOK && rangeOk) === 'true') { // si ambos da true entra
-            alert('Cohete listo')
-            btnLaunch.removeAttribute('disabled'); //activa el boton de lanzamiento
-            addDisabled(checkbox); //desactiva las caja de check y rango
-            addDisabled(range);
-            activarFunClickCheckbox = false //vuelve a poner en falso la variable para ya no entre a la función cada vez que se da click
-        }
-    }
-}
-
-/*Funcion que controla que todas las cajas esten en check*/
+/*Funcion que controla que todas las cajas estén en check*/
 function controlCheck(){
     let controlDeCheck = 0;
     for (let i = 0; i < checkbox.length; i++) {  /* cada vez que entra al bucle controla que las casillas esten todas en true*/
@@ -71,7 +78,7 @@ function controlCheck(){
     }
     return controlDeCheck === 6 ? 'true' : 'false'; /*si el contador es igual a la cantidad de casillas devuelve true, si no false*/
 }
-/*Funcion que controla que todas las cajas de control de rango esten en 100*/
+/*Funcion que controla que todas las cajas de control de rango estén en 100*/
 function controlRango(){
     let controlDeRango = 0;
     for (let i = 0; i < range.length; i++){
